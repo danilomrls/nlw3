2e23342095a6930c16ed74c7e8be77220cf75499
Danilo Meireles <danilopalhetam@gmail.com>
2020-10-21 09:44:24 -0300

    installing the nodemon

--- src/server.js
+++ src/server.js
@@ -1,1 +1,13 @@
-console.log('olá')
+//import dependencies
+const express = require('express');
+
+//initianting the express
+const server = express();
+
+//create a route
+server.get('/', () => {
+    console.log('Oi')
+})
+
+//turn on the server
+server.listen(5500)
