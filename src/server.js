cf895474446c9ffa0eda838a9a930b4737aafef7
Danilo Meireles <danilopalhetam@gmail.com>
2020-10-21 10:41:40 -0300

    installing and configuring the handlebars

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
 //import dependencies
 const express = require('express');
+const path = require('path');
 
 //initianting the express
 const server = express();
+server
+//using the static files
+.use(express.static('public'))
+
+//configure template engines
+.set('views', path.join(__dirname, "views"))
+.set('view engine', 'hbs')
 
 //create a route
-server.get('/', () => {
-    console.log('Oi')
+.get('/', (request, response) => {    
+    return response.render('index')
 })
 
 //turn on the server
