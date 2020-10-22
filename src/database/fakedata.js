c1940aad976c2dd1bb7595773a5aaef703aac4bd
Danilo Meireles <danilopalhetam@gmail.com>
2020-10-22 07:19:12 -0300

    using the back-end to create the popups on the orphanages page

---
+++ src/database/fakedata.js
@@ -0,0 +1,33 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-3.7338316",
+        lng: "-38.5347852",
+        name: "Lar das meninas",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
+        images: [
+            "https://images.unsplash.com/photo-1598454444299-62f9d5d5dc48?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1598136490941-30d885318abd?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions:"Venha como se sentir a vontade e traga muito amor e paciência para dar",
+        opening_hours: "Horário de visitas das 8h até 18h",
+        open_on_weekends: "1"
+    },
+
+    {
+        id: 2,
+        lat: "-3.7348316",
+        lng: "-38.5447852",
+        name: "Lar dos meninos",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
+        images: [
+            "https://images.unsplash.com/photo-1598454444299-62f9d5d5dc48?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1598136490941-30d885318abd?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions:"Venha como se sentir a vontade e traga muito amor e paciência para dar",
+        opening_hours: "Horário de visitas das 8h até 18h",
+        open_on_weekends: "0"
+    },
+]
