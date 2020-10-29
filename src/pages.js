8a9ed18b8e03cacd0eb28134e7a39acfe86a445f
Danilo Meireles <danilopalhetam@gmail.com>
2020-10-29 05:49:09 -0300

    configuring the session open on weekends and addind the whatsapp api

--- src/pages.js
+++ src/pages.js
@@ -19,6 +19,11 @@ module.exports = {
 
             orphanage.images = orphanage.images.split(",")
             orphanage.firstImage = orphanage.images[0]
+            if(orphanage.open_on_weekends == "0") {
+                orphanage.open_on_weekends = false
+            } else {
+                orphanage.open_on_weekends = true
+            }
 
             return res.render('orphanage', { orphanage })
         } catch (error) {
